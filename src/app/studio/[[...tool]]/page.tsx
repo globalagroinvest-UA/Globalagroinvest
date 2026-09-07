ba04d55bad901d77dad5c34fda60a172e4488cbf
globalagroinvest-UA <global.agro.invest@gmail.com>
2026-09-07 16:14:11 +0300

    Update page.tsx

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,9 @@
+'use client'
 import { NextStudio } from "next-sanity/studio";
 
 import config from "../../../../sanity.config";
 
-export { metadata, viewport } from "next-sanity/studio";
+// *** export { metadata, viewport } from "next-sanity/studio";
 
 // The Studio is a fully client-rendered SPA; there is nothing to prerender
 // and every request should get the same static shell.
